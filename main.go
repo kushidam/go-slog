1afe18d1ab6ba9f18d8b948e4f7e4e583c08d900
kushidam <131370475+kushidam@users.noreply.github.com>
2023-09-10 20:37:22 +0900

    init

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"golang.org/x/exp/slog"
+)
+
+func main() {
+	// 各レベル出力メソッド
+	slog.Info("hello", "count", 1 )
+	slog.Debug("hello", "count", 2 )
+	slog.Warn("hello", "count", 3)
+
+	err := errors.New("this is error")
+	slog.Error("message", err, "count", 4)
+
+	// 構造化レコードをテキスト形式ロガー作成
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	logger.Info("hello", "count", 5)
+
+	// logパッケージへのdefault設定
+	log.Println("log package SetDefault before")
+	slog.SetDefault(logger)
+	log.Println("log package SetDefault after")
+
+	// JSON形式出力
+	logger_json := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger_json.Info("hello json", "count", 6)
+
+	// レベル設定：ログ イベントの重要性または重大度を表す整数
+	var programLevel = new(slog.LevelVar) // デフォルトの情報
+	h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: programLevel})
+	slog.SetDefault(slog.New(h))
+	programLevel.Set(slog.LevelDebug)
+}
